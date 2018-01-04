897e0cf47eb523d76f27a275ab5e5d4f1c765f8c
Dmitry <voronov-d88@yandex.ru>
2018-01-04 10:53:54 +0000

    Create template site

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,6 +1,12 @@
 from flask_app.flask_server import app
+from flask import render_template
 
 
 @app.route("/")
 def hello():
-    return "Hello Differ!"
+    return render_template('differ.html')
+
+
+@app.route("/result", methods=['POST'])
+def show_result_diff():
+    return render_template('result.html')
